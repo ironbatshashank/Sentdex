a21c2d4dc638a07d34c4f53492b8d69d7f594b13
Shashank Srivastava <shashanksri65@gmail.com>
2018-08-07 14:55:50 +0530

    Test

--- ML.py
+++ ML.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import Quandl
 
-df = Quandl.get('')
+df = Quandl.get('WIKI/GOOGL')
+
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+df['HL__PCT'] = {df['Adj. High'] - df['Adj. Close']} / df['Adj.Close'] * 100.0
+df['PCT_change'] = {df['Adj. High'] - df['Adj. Open']} / df['Adj.Open'] * 100.0
+
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj']]
